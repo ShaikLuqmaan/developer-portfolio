a69f7c75f8e7c4d41047bb5754058a5b0aaa5871
Luqmaan Shaik <luqmaanshaik@gmail.com>
2024-01-20 22:25:21 -0500

    intial setup

---
+++ constants/data.js
@@ -0,0 +1,100 @@
+const userData = {
+  githubUsername: "manuarora700",
+  name: "Luqmaan Shaik",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "shaikl@uwindsor.ca",
+  phone: "+1 (226) 961 8969",
+  address: "Windsor, Ontario, Canada.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Masters in Applied Computing",
+      company: "University of Windsor",
+      year: "2023 - 2024",
+      companyLink: "https://www.uwindsor.ca/",
+      desc: "Currently pursuing a Master of Applied Computing degree at the University of Windsor",
+    },
+    {
+      title: "Software Engineer III",
+      company: "Zemoso Technologies",
+      year: "2020 - 2023",
+      companyLink: "https://www.zemosolabs.com/",
+      desc: "Designed and developed client-side and server-side SDKs in JS, React,  Node and Java programming languages and contributed on a privacy-related web application",
+    },
+    {
+      title: "Internship",
+      company: "ValueMomentum",
+      year: "2019",
+      companyLink: "https://www.valuemomentum.com/",
+      desc: "Actively contributed to the development of UI pages for various features using React",
+    },
+    {
+      title: "Bachelor of Engineering",
+      company: "Osmania University",
+      year: "2016 - 2020",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering with a CGPA of 8.7",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/shaik-luqmaan/",
+    github: "https://github.com/ShaikLuqmaan",
+  },
+};
+
+export default userData;
